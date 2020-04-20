7c3543f8eade02bc40b2337210f887fea9415036
Viet Son <vson.ng91@gmail.com>
2020-04-20 21:14:50 +0200

    1st commit in this project

---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,18 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController
+{
+    /**
+     * @Route("/")
+     */
+    public function homepage(){
+        return new Response("this is a new page");
+    }
+}
