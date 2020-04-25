af13b794d37631326aba82b7c3c958db98f20bce
Viet Son <vson.ng91@gmail.com>
2020-04-25 14:03:12 +0200

    Learn AdapterInterface cache and env

---
+++ src/Controller/CommentController.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CommentController extends AbstractController
+{
+    /**
+     * @Route("/comments/{id}/vote/{direction<up|down>}", name="comment_vote", methods="POST")
+     */
+    public function commentVote($id, $direction){
+
+        if($direction ==='up'){
+            $currentVoteCount = rand(7,100);
+        }else{
+            $currentVoteCount = rand(0,5);
+        }
+
+
+        return $this->json(['votes'=>$currentVoteCount]);
+    }
+}
--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -4,24 +4,49 @@
 namespace App\Controller;
 
 
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController
+class QuestionController extends AbstractController
 {
     /**
      * @Route("/")
      */
     public function homepage(){
-        return new Response("this is a new page");
+        return $this->render('question/homepage.html.twig');
     }
 
     /**
      * @Route("question/{slug}")
      */
-    public function show($slug){
+    public function show($slug, MarkdownParserInterface $markdown, AdapterInterface $cache){
+        $answers = ['answers 1', 'answer 2', 'answer 3','answer 4'];
+
+        $questionContent = <<<EOF
+Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
+lorem proident [beef ribs](https://google.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
+labore minim pork belly spare ribs cupim short loin in. Elit l'exercitation eiusmod dolore cow
+turkey shank eu pork belly meatball non cupim.zaeaeaz
+EOF;
+        $item = $cache->getItem('markdown_'.md5($questionContent));
+
+        // check if the requested item is found in the cache. To check it, we use methode isHit()
+        if (!$item->isHit()){
+            $questionContent = $markdown->transformMarkdown($questionContent);
+            $item->set($questionContent);
+            $cache->save($item);
+        }
+        $questionContent = $item->get();
+
+       // dump($cache);die();
+
         return $this->render('question/show.html.twig',[
-            'question' => ucwords(str_replace('-',' ', $slug))
+            'question' => ucwords(str_replace('-',' ', $slug)),
+                'questionContent' => $questionContent,
+                'answers' => $answers
             ]
         );
     }
