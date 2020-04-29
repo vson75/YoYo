c02f7a58a48eaf992c2866121d320a72c6ddf656
Viet Son <vson.ng91@gmail.com>
2020-04-29 11:12:05 +0200

    Add Post controller and twig

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,16 +3,14 @@
 
 namespace App\Controller;
 
-
 use App\Service\MarkdownHelper;
-use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class QuestionController extends AbstractController
+class PostController extends AbstractController
 {
+
     /**
      * @Route("/")
      */
@@ -21,29 +19,29 @@ class QuestionController extends AbstractController
     }
 
     /**
-     * @Route("question/{slug}")
+     * @Route("post/{slug}")
      * @param $slug
      * @param MarkdownHelper $markdownHelper
      * @return Response
      */
     public function show($slug, MarkdownHelper $markdownHelper){
-        $answers = ['answers 1', 'answer 2', 'answer 3','answer 4'];
+        $comment = ['answers 1', 'answer 2', 'answer 3','answer 4'];
 
-        $questionContent = <<<EOF
+        $postContent = <<<EOF
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://google.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit l'exercitation eiusmod dolore cow
 turkey shank eu pork belly meatball non cupim.zae
 EOF;
 
-        $questionContent = $markdownHelper->parse($questionContent);
+        $postContent = $markdownHelper->parse($postContent);
 
-       // dump($cache);die();
+        // dump($cache);die();
 
-        return $this->render('question/show.html.twig',[
-            'title' => ucwords(str_replace('-',' ', $slug)),
-                'questionContent' => $questionContent,
-                'answers' => $answers
+        return $this->render('post/show_post.html.twig',[
+                'title' => ucwords(str_replace('-',' ', $slug)),
+                'postContent' => $postContent,
+                'comment' => $comment
             ]
         );
     }
