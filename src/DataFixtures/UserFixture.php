838219531cd319269381c4d012f9de0718fc22dd
Viet Son <vson.ng91@gmail.com>
2020-05-06 09:49:55 +0200

    add User, login form and modify navbar

---
+++ src/DataFixtures/UserFixture.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Faker;
+
+class UserFixture extends Fixture
+{
+    protected $faker;
+    public function load(ObjectManager $manager)
+    {
+        $this->faker = Faker\Factory::create('fr_FR');
+        // $product = new Product();
+        // $manager->persist($product);
+        for ($i = 0; $i < 20; $i++) {
+            $user = new User();
+            $user->setEmail("yoyo".$i."@gmail.com");
+            $user->setFirstname($this->faker->firstName);
+            $user->setLastname($this->faker->lastName);
+            $user->setPassword("1234");
+            $manager->persist($user);
+            $manager->flush();
+        }
+
+        $manager->flush();
+    }
+}
