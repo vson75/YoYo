3523396edf4b65a4ced2daf5d8f9c92b44a426a6
Viet Son <vson.ng91@gmail.com>
2020-05-17 09:50:35 +0200

    Add Mailer composer and email in case registration

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,22 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Form\UserRegistrationFormType;
 use App\Security\LoginFormAuthenticator;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
-use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
+
+
 
 class SecurityController extends AbstractController
 {
@@ -40,8 +46,16 @@ class SecurityController extends AbstractController
 
     /**
      * @Route("/registration", name="app_registration")
+     * @param EntityManagerInterface $em
+     * @param MailerInterface $mailer
+     * @param Request $request
+     * @param UserPasswordEncoderInterface $passwordEncoder
+     * @param GuardAuthenticatorHandler $guardHandler
+     * @param LoginFormAuthenticator $formAuthenticator
+     * @return \Symfony\Component\HttpFoundation\Response|null
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
-    public function registration(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator, ValidatorInterface $validator){
+    public function registration(EntityManagerInterface $em, MailerInterface $mailer, Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginFormAuthenticator $formAuthenticator){
         $form = $this->createForm(UserRegistrationFormType::class);
         $form->handleRequest($request);
 
@@ -49,21 +63,44 @@ class SecurityController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             /** @var User $user */
             $user = $form->getData();
+            $userEmail = $user->getEmail();
+            $repo = $em->getRepository(User::class);
+            $checkEmail = $repo->findBy([
+                'email' => $userEmail
+            ]);
+       //  dd(!empty($checkEmail));
+            if(!empty($checkEmail)){
+                $this->addFlash('notice', 'Email của bạn đã được sử dụng. Bạn có thể reset password để tạo mật khẩu mới');
+                return $this->render('security/register.html.twig', [
+                    'registrationForm' => $form->createView(),
+                ]);
 
-            $user->setPassword($passwordEncoder->encodePassword(
-                $user,
-                $user->getPassword()
-            ));
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($user);
-            $em->flush();
+            }else{
+                $user->setPassword($passwordEncoder->encodePassword(
+                    $user,
+                    $user->getPassword()
+                ));
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($user);
+                $em->flush();
 
-            return $guardHandler->authenticateUserAndHandleSuccess(
-                $user,
-                $request,
-                $formAuthenticator,
-                'main'
-            );
+                // after insert new user in our DB, send mail congratulation
+                $email = new TemplatedEmail();
+                $email  ->from('YoYo@gmail.com')
+                    ->to($user->getEmail())
+                    ->subject("Welcome in YoYo {$user->getFirstname()}")
+                    ->htmlTemplate('email/email_welcome.html.twig');
+                $mailer->send($email);
+
+
+                return $guardHandler->authenticateUserAndHandleSuccess(
+                    $user,
+                    $request,
+                    $formAuthenticator,
+                    'main'
+                );
+            }
+
         }
         return $this->render('security/register.html.twig', [
             'registrationForm' => $form->createView(),
